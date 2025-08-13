02a4b8fb521e46916afc431b7ea3fc14620134ac
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-08-13 15:17:49 +0300

    Add Privy authentication integration and update dependencies
    - Integrated Privy authentication in App component with login functionality.
    - Wrapped App component in PrivyProvider in main.tsx for authentication context.
    - Updated package.json and package-lock.json to include @privy-io/react-auth dependency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useLogin, usePrivy } from '@privy-io/react-auth'
 import Game from './Game'
 import './App.css'
 
 function App() {
+  const { ready, authenticated} = usePrivy();
+  const { login } = useLogin();
+  // Disable login when Privy is not ready or the user is already authenticated
+  const disableLogin = !ready || (ready && authenticated);
+
+
+  if (!ready) {
+    return <div>Loading...</div>
+  }
+
+  // Now it's safe to use other Privy hooks and state
   return (
     <div className="App">
+      <button disabled={disableLogin} onClick={login}>
+            Log in
+        </button>
       <Game />
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <PrivyProvider
+      appId={import.meta.env.VITE_PRIVY_APP_ID}
+      // clientId={import.meta.env.VITE_PRIVY_CLIENT_ID}
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        loginMethodsAndOrder: {
+          primary: ['privy:cmd8euall0037le0my79qpz42'],
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </React.StrictMode>
+);
