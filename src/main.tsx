db05eea18393622d1719c60c460280bd63856875
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-08-13 15:52:47 +0300

    Remove commented-out clientId prop from PrivyProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,6 @@ root.render(
   <React.StrictMode>
     <PrivyProvider
       appId={import.meta.env.VITE_PRIVY_APP_ID}
-      // clientId={import.meta.env.VITE_PRIVY_CLIENT_ID}
       config={{
         // Create embedded wallets for users who don't have a wallet
         loginMethodsAndOrder: {
